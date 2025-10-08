df4eba0d091e4776711b43c1508dd8f9b25c4e44
EdgeYu <Y770177948@outlook.com>
2025-10-08 15:59:55 +0800

    chore: 更新站点域名至edgeyu.top

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://edgeyu.github.io',
+	site: 'https://edgeyu.top',
 	integrations: [mdx(), sitemap()],
 });
